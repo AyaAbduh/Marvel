4c9cbde02ca36301c780fc7d311394a6d9172772
Aya Abdo <aya.yoya962015@gmail.com>
2025-02-13 10:59:25 +0200

    integration with compose

--- app/src/main/java/com/example/marvel/presentation/characters/HomeActivity.kt
+++ app/src/main/java/com/example/marvel/presentation/characters/HomeActivity.kt
@@ -1,11 +1,23 @@
 package com.example.marvel.presentation.characters
 
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
+import android.widget.ImageView
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.ListItem
 import androidx.compose.material3.Scaffold
@@ -15,14 +27,25 @@ import androidx.compose.runtime.State
 import androidx.compose.runtime.getValue
 import androidx.paging.compose.collectAsLazyPagingItems
 import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.layout.VerticalAlignmentLine
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.compose.ui.viewinterop.AndroidView
 import androidx.lifecycle.lifecycleScope
 import androidx.paging.PagingData
 import androidx.paging.compose.LazyPagingItems
+import com.bumptech.glide.Glide
 import com.example.marvel.data.Character
 import com.example.marvel.presentation.characters.ui.theme.MarvelTheme
+import com.example.marvel.presentation.singlecharacter.CharacterDetailsActivity
 import com.example.marvel.presentation.viewmodel.HomeViewModel
 import kotlinx.coroutines.launch
 
@@ -35,8 +58,7 @@ class HomeActivity : ComponentActivity() {
         setContent {
             MarvelTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    Home(
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -46,14 +68,39 @@ class HomeActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
+fun Home( modifier: Modifier = Modifier) {
      var homeViewModel  = HomeViewModel()
     val pagingItems: LazyPagingItems<Character>  = homeViewModel.items.collectAsLazyPagingItems()
 
 
     LazyColumn {
         items(pagingItems.itemCount) { index ->
-            pagingItems[index]?.name?.let { Text(text = it, modifier = Modifier.padding(8.dp)) }
+
+            Box{
+                //Image View
+                AndroidView(
+                    factory = { context ->
+                        ImageView(context).apply {
+                            Glide.with(context)
+                                .load(pagingItems[index]?.thumbnail?.path?.replace("http","https")+"."+pagingItems[index]?.thumbnail?.extension) // URL or resource for the image
+                                .into(this)
+                            scaleType = ImageView.ScaleType.FIT_XY  // Apply scale type to the ImageView
+                            setOnClickListener {
+                                onImageClick(context,pagingItems[index]?.id!!) // Trigger the provided lambda function
+                            }
+
+                        }
+                    },
+                    modifier = Modifier.fillMaxSize().height(200.dp), // Modify size of Image
+                    )
+
+                pagingItems[index]?.name?.let {
+                    Text(text = it, modifier = Modifier.padding(top = 100.dp, start = 16.dp).background(Color.White), fontSize = 20.sp, fontWeight = FontWeight.Bold)
+                }
+
+
+            }
+
 
 
         }
@@ -61,10 +108,19 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 
 }
 
+fun onImageClick(context: Context,id:Int) {
+
+    val intent = Intent(context, CharacterDetailsActivity::class.java)
+    intent.putExtra("id", id)
+
+    context.startActivity(intent) // Start the activity
+
+}
+
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     MarvelTheme {
-        Greeting("Android")
+        Home()
     }
 }
